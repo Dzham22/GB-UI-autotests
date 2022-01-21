a071492231896aea545188db8793746725ff2131
Dzham22 <dzham22@yandex.ru>
2022-01-21 12:43:38 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * GeekBrains/UI-autotests/Lesson2/!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "GeekBrains/UI-autotests/Lesson2/!" );
+    }
+}
